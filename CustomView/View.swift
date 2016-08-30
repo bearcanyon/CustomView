1ac7d598e20e564efbbfecb5f34bab43d7013dd7
KumagaiNaoki <j.a.r.n0405@gmail.com>
2016-08-30 10:25:45 +0900

    CustomViewの登録とoutletとactionのつけ方

---
+++ CustomView/View.swift
@@ -0,0 +1,35 @@
+//
+//  View.swift
+//  CustomView
+//
+//  Created by KumagaiNaoki on 2016/08/30.
+//  Copyright © 2016年 KumagaiNaoki. All rights reserved.
+//
+
+import UIKit
+
+class View: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var myButton: UIButton!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // xibファイル読み込み
+        NSBundle.mainBundle().loadNibNamed("View", owner: self, options: nil)
+        
+        // Viewの大きさを定義
+        contentView.frame = frame
+        
+        // xibファイルのViewをカスタムViewクラスに追加する
+        addSubview(contentView)
+        
+        
+        myButton.setTitle("hoge", forState: .Normal)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- CustomView/ViewController.swift
+++ CustomView/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let view = View(frame: self.view.frame)
+        self.view.addSubview(view)
+        
+        view.myButton.addTarget(self, action: #selector(ViewController.tapButton), forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
+    func tapButton() {
+        print("\(#function),\(#line)")
     }
 
     override func didReceiveMemoryWarning() {
